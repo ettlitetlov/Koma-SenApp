2becabfefbe960ad8f4c8441b8078febb221cb99
Skankalicious <bollbergo@gmail.com>
2015-11-20 09:36:53 +0100

    Kanske lade till så att activity meeting fungerar

---
+++ app/src/main/java/com/example/josfl888/senapp/SetMeeting.java
@@ -0,0 +1,14 @@
+package com.example.josfl888.senapp;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+public class SetMeeting extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_meeting);
+    }
+
+}
